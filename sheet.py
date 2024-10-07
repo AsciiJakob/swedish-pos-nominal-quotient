78f044a13b0e777a9121b59f1e7ae2ec0d5a6b3d
Jakob <jakobjakobjakob2@gmail.com>
2024-10-07 15:49:40 +0200

    swedish decimal formatting in csv file

--- sheet.py
+++ sheet.py
@@ -1,12 +1,18 @@
 import json
 import csv
+import locale
 
+# since spreadsheets expect "," instead of "." as a decimal indicator in Sweden.
+locale.setlocale(locale.LC_NUMERIC, "sv_SE.UTF-8")
 
 model = "kb_bert"
 
+def comma(value):
+    return locale.format_string("%.12f", value)
+
 def generate_sheet():
     with open("output/sheet.csv", "w", newline="") as csvfile:
-        writer = csv.writer(csvfile)
+        writer = csv.writer(csvfile, delimiter=";")
         writer.writerow(["Fil","ID","Nominalkvot","Enkel nominalkvot", "Antal ord","Genomsnittlig meningslängd"])
 
         try:
@@ -19,16 +25,15 @@ def generate_sheet():
         for file_data in computedData:
             filename = file_data["filename"]
             for text_data in file_data["texts"]:
-                if filename == "15 E" and text_data["id"] == "C242":
-                    print("quotient: "+ str(text_data["full_nominal_quotient"]))
                 writer.writerow([
                     filename,
                     text_data["id"],
-                    text_data["full_nominal_quotient"],
-                    text_data["simple_nominal_quotient"],
+                    comma(text_data["full_nominal_quotient"]),
+                    comma(text_data["simple_nominal_quotient"]),
                     text_data["word_count"],
-                    text_data["mean_sentence_length"]
+                    comma(text_data["mean_sentence_length"])
                 ])
+            writer.writerow([]) # one empty row for new file
 
 
 
