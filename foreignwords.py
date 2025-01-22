c5e6c0f8314de286a57a3f52a5f037ac7c4896c2
Jakob <jakobjakobjakob2@gmail.com>
2025-01-22 20:27:11 +0100

    use snake case

--- foreignwords.py
+++ foreignwords.py
@@ -3,20 +3,20 @@ from os import listdir
 from os.path import isfile, join
 
 output = []
-folderPath = "output/"
-only_files = [f for f in listdir(folderPath) if isfile(join(folderPath, f))]
+folder_path = "output/"
+only_files = [f for f in listdir(folder_path) if isfile(join(folder_path, f))]
 for file in only_files:
     if (file.startswith("computed_") and file.endswith(".json")):
         model = file.split("computed_")[1].split(".json")[0]
         with open("output/"+model+".csv", "w", newline="") as csvfile:
             try:
                 with open("output/computed_"+model+".json") as f:
-                    computedData = json.load(f)
+                    computed_data = json.load(f)
             except:
                 print("missing file for model", model)
                 exit(1)
             
-            for file_data in computedData:
+            for file_data in computed_data:
                 filename = file_data["filename"]
                 for text in file_data["texts"]:
                     for sentence in text["filtered_sentences"]:
@@ -24,6 +24,6 @@ for file in only_files:
                             if token["entity_group"] == "UO":
                                 output.append(f"{token["word"]} [{text["id"]}]\n")
 
-with open(folderPath+"foreign_words.txt", "w", encoding="utf-8") as file:
+with open(folder_path+"foreign_words.txt", "w", encoding="utf-8") as file:
     for item in output:
         file.write(item)
